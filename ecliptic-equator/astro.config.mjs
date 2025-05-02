88c58865c685b9ab092e9ac03427e5d57980539d
astromarb <marvinlopezacevedo.personal@gmail.com>
2025-05-01 19:31:51 -0500

    feat: update base path in astro.config.mjs and adjust workspace settings
    Signed-off-by: astromarb <marvinlopezacevedo.personal@gmail.com>

--- ecliptic-equator/astro.config.mjs
+++ ecliptic-equator/astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config'
 
 export default defineConfig({
   site: 'https://astromarb.github.io',
-  base: '/ecliptic-equator/src',
+  base: '/ecliptic-equator',
 })
