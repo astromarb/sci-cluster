7f61269248d58d7609b02616c8e7af32870eadcf
astromarb <marvinlopezacevedo.personal@gmail.com>
2025-05-01 19:54:08 -0500

    feat: rename package.json and update workspace.xml for improved deployment settings
    Signed-off-by: astromarb <marvinlopezacevedo.personal@gmail.com>

--- ecliptic-equator/astro.config.mjs
+++ ecliptic-equator/astro.config.mjs
@@ -1,6 +1,8 @@
-import { defineConfig } from 'astro/config'
+// @ts-check
+import { defineConfig } from 'astro/config';
 
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://astromarb.github.io',
-  base: '',
-})
+    site: 'https://astromarb.github.io',
+    base: '',
+});
