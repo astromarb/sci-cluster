882eca89da64f7f6dbabeee3e2c9a1a347dbda2c
astromarb <marvinlopezacevedo.personal@gmail.com>
2026-02-16 18:23:03 -0600

    Add comparison utility functions to __all__ for enhanced data analysis capabilities
    Signed-off-by: astromarb <marvinlopezacevedo.personal@gmail.com>

--- sci_helpers/__init__.py
+++ sci_helpers/__init__.py
@@ -1,4 +1,5 @@
 from .csv_to_samples import csv_to_samples_list
 from .mc_wrangler import wrangle_dataframe, wrangle_excel
+from .compare_wrangled import compare_wrangled, compare_wrangled_simple
 
-__all__ = ["csv_to_samples_list", "wrangle_dataframe", "wrangle_excel"]
+__all__ = ["csv_to_samples_list", "wrangle_dataframe", "wrangle_excel", "compare_wrangled", "compare_wrangled_simple"]
